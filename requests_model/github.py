97235eb5f7ade7ba2366f6250995a5b60c5800dc
AndyJCheng <andy@example.com>
2018-07-06 22:49:49 +0800

    parameters

--- requests_model/github.py
+++ requests_model/github.py
@@ -5,6 +5,7 @@
 @license: Apache Licence 
 @file: github.py
 @time: 2018/7/2 22:59
+@doc: https://github.com/jian-en/imooc-requests
 """
 import requests
 import json
@@ -15,10 +16,20 @@ def build_uri(endpoint):
     return '/'.join([url, endpoint])
 
 
+def better_print(json_str):
+    return json.dumps(json.loads(json_str), indent=4)
+
+
 def request_method():
     response = requests.get(build_uri('users/AndyJCheng'))
     print(json.dumps(json.loads(response.text), indent=4))
 
 
+def request_parameters():
+    response = requests.get(build_uri('users'), params={'since': 11})
+    print(better_print(response.text))
+    print(response.url)
+
+
 if __name__ == '__main__':
-    request_method()
+    request_parameters()
